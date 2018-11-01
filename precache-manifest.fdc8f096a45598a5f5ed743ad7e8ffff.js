2cd93ddb1f8e808ad3968dd835f5de5700717bb4
brurez <brunoderezende@hotmail.com>
2018-11-01 09:00:59 -0300

    Updates

---
+++ precache-manifest.fdc8f096a45598a5f5ed743ad7e8ffff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c526a9417fb0844bce405c6958b9028",
+    "url": "/static/media/truck.0c526a94.png"
+  },
+  {
+    "revision": "b2ca867ebcd505e390859275d29a0665",
+    "url": "/static/media/taxi.b2ca867e.png"
+  },
+  {
+    "revision": "a03ad39a722b13f37ed7905a5e9e079b",
+    "url": "/static/media/road.a03ad39a.png"
+  },
+  {
+    "revision": "a39891fafd91f7634321aea9f850d1d0",
+    "url": "/static/media/Car.a39891fa.png"
+  },
+  {
+    "revision": "5498a2901c0a9417e00d2230f408c57e",
+    "url": "/static/media/Audi.5498a290.png"
+  },
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "91927030b20c6d7fea0d",
+    "url": "/static/js/main.91927030.chunk.js"
+  },
+  {
+    "revision": "91927030b20c6d7fea0d",
+    "url": "/static/css/main.e5c1c75d.chunk.css"
+  },
+  {
+    "revision": "4eac96c2dc6eccd42004014c6c356313",
+    "url": "/index.html"
+  }
+];
